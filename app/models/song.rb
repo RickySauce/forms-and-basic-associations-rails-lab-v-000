3be4cefb5edd0610a15d57ff6be34d0a4e12cf92
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-20 12:20:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -26,7 +26,7 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents
-    self.notes.collect {|note|note.content unless note.content.empty?}.collapse
+    self.notes.collect {|note|note.content unless note.content.empty?}.compact
   end
 
 end
