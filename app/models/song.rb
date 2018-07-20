27d46f33ce134cf415b4ae5f69d9cb6037625580
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-20 12:16:38 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -20,7 +20,7 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents=(contents)
-    contents.each do |content| 
+    contents.each do |content|
       Self.notes << Note.find_or_create_by(content: content)
     end
   end
